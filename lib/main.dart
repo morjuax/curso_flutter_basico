038ad27f9afa30c391a9f4c29fe34a9d10122613
Juan Moreno <morjua19@gmail.com>
2021-01-24 15:49:38 -0300

    [UPD 2] models y modularizacion de widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curso_flutter_basico/models/person_model.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,32 +6,49 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  PersonModel person1 = PersonModel(
+    name: 'Juan Moreno',
+    description: 'Ejemplo de modelo',
+    iconLeft: Icons.person,
+    iconRight: Icons.message,
+    nickName: 'morjuax'
+  );
+
+  // error nickname data === null
+
+  Widget customListTile(PersonModel person) {
+    return ListTile(
+      leading: Icon(person.iconLeft),
+      trailing: Icon(person.iconRight),
+      title: Text(person.nickName),
+      subtitle: Text(person.description),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        appBar: AppBar(
-          title: Text('Matanga app'),
-        ),
-        body: Container(
-            child: Center(
-              child: Text(
-                'Mi primer widget',
-                textAlign: TextAlign.center,
-                style: TextStyle(decoration: TextDecoration.none),
-              ),
-            )
-        ),
-      )
-    );
+        home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
+          appBar: AppBar(
+            actions: [
+              IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+              // IconButton(icon: Icon(Icons.image), onPressed: () {}),
+              // IconButton(icon: Icon(Icons.message), onPressed: () {})
+            ],
+            title: Text('Matanga app'),
+          ),
+          body: Container(
+            child: customListTile(person1),
+          ),
+        ));
   }
 }
-
